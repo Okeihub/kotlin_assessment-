cb2e554881140a60f2860267331f56903770e47c
Okeihub <85238503+Okeihub@users.noreply.github.com>
2021-06-29 16:50:43 +0100

    Add files via upload

---
+++ untitled4/src/main/kotlin/main.kt
@@ -0,0 +1,19 @@
+fun main() {
+
+    println("PrintSpace")
+}
+
+    fun printSpace(){
+        var a: Int = 0
+        var b: Int = 0
+        var c: Int = 0
+
+        when {
+            b-a == c-b -> println("true")
+            else -> println("false")
+
+        }
+
+
+
+    }
